a67b0242ca33ada2b38ef024f3fca461fae9ad85
ko5ba <104355972+ko5ba@users.noreply.github.com>
2024-11-08 23:06:03 +0300

    feat: added user error message and fixed form handling for invalid city input

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@ import WeatherSummary from './components/WeatherSummary.vue'
 import Highlights from './components/Highlights.vue'
 import Coords from './components/Coords.vue'
 import Humidity from './components/Humidity.vue'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { API_KEY, API_URL } from './constants'
+import { capitalizeFirstLatter } from './utils'
 
 
 const weatherInfo = ref(null);
@@ -17,6 +18,8 @@ function getWeather() {
 }
 
 onMounted(getWeather);
+
+const isError = computed(() => weatherInfo.value?.cod !== 200)
 </script>
 
 <template>
@@ -25,7 +28,7 @@ onMounted(getWeather);
         <div class="container">
           <div class="laptop">
             <div class="sections">
-              <section class="section section-left">
+              <section :class="['section', 'section-left', { 'section-error': isError }]">
                 <div class="info">
                   <div class="city-inner">
                     <input 
@@ -35,14 +38,22 @@ onMounted(getWeather);
                     class="search"
                     >
                   </div>
-                  <WeatherSummary :weatherInfo="weatherInfo"/>
+                  <WeatherSummary v-if="!isError" :weatherInfo="weatherInfo"/>
+                  <div v-else class="error">
+                    <div class="error-title">
+                      Ooops! Something went wrong
+                    </div>
+                    <div v-if="weatherInfo?.message" class="error-message">
+                        {{ capitalizeFirstLatter(weatherInfo?.message) }}
+                    </div>
+                  </div>
                 </div>
               </section>
-              <section class="section section-right">
+              <section v-if="!isError" class="section section-right">
                 <Highlights :weatherInfo="weatherInfo" />
               </section> 
             </div>
-            <div v-if="weatherInfo?.weather" class="sections">
+            <div v-if="!isError" class="sections">
               <Coords :coord="weatherInfo.coord"/>
               <Humidity :humidity="weatherInfo.main.humidity"/>
             </div>
@@ -84,6 +95,10 @@ onMounted(getWeather);
     width: 100%
     padding-right: 0
 
+  &.section-error 
+    min-width: 235px
+    width: auto
+    padding-right: 0
 .section-right
   width: 70%
   padding-left: 10px
@@ -134,4 +149,13 @@ onMounted(getWeather);
 
   @media (max-width: 767px)
     width: 100%
+.error 
+  padding-top: 20px
+
+  &-title 
+    font-size: 18px
+    font-weight: 700
+
+  &-message 
+    padding-top: 10px
 </style>
--- src/components/WeatherSummary.vue
+++ src/components/WeatherSummary.vue
@@ -12,8 +12,8 @@ const today = new Date().toLocaleString('en-EN', { weekday: 'long', year: 'numer
 </script>
 
 <template>
-  <div v-if="weatherInfo?.weather" class="summary">
-    <div :style="`background-image: url('/src/assets/img/weather-main/${weatherInfo?.weather[0].description}.png');`" class="pic-main"></div>
+  <div class="summary">
+    <div :style="`background-image: url('/weather-main/${weatherInfo?.weather[0].description}.png');`" class="pic-main"></div>
     <div class="weather">
       <div class="temp">
         {{ Math.round(weatherInfo?.main?.temp) }} °C
