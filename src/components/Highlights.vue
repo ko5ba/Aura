3080341128796a7b1495e5c043b6543ffb9f04bf
ko5ba <104355972+ko5ba@users.noreply.github.com>
2024-11-08 21:24:28 +0300

    feat: populate all components with data from server responses

--- src/components/Highlights.vue
+++ src/components/Highlights.vue
@@ -1,8 +1,27 @@
 <script setup>
+import { computed } from 'vue'
+import { getPressureMm, getTime } from '../utils'
+
+const props = defineProps({
+  weatherInfo: {
+    type: [Object, null],
+    required: true
+  }
+})
+
+const timezone = computed(() => props.weatherInfo?.timezone)
+
+const sunriseTime = computed(() => {
+  return getTime(props.weatherInfo?.sys?.sunrise + timezone.value)
+})
+
+const sunsetTime = computed(() => {
+  return getTime(props.weatherInfo?.sys?.sunset + timezone.value)
+})
 </script>
 
 <template>
-  <div class="section highlights">
+  <div v-if="weatherInfo?.weather" class="section highlights">
     <div class="title">
       Today's Highlights
     </div>
@@ -17,7 +36,7 @@
             <div class="card-justify">
               <div class="info-main">
                 <div class="info-main-num">
-                  3.6
+                  {{ weatherInfo?.wind?.speed }}
                 </div>
                 <div class="info-main-text">
                   m/s
@@ -25,7 +44,7 @@
               </div>
               <div class="info-main">
                 <div class="info-main-num">
-                  350
+                  {{ weatherInfo?.wind?.deg }}
                 </div>
                 <div class="info-main-text">
                   deg
@@ -39,9 +58,9 @@
             Wind gusts
           </div>
           <div class="card-small-info">
-            <div class="card-small-data">
+            <div v-if="weatherInfo?.wind?.gust" class="card-small-data">
               <div class="info-main-num">
-                8.4
+                {{ Math.roud(weatherInfo?.wind?.gust) }}
               </div>
               <div class="info-main-text">
                 m/s
@@ -69,7 +88,7 @@
             <div class="card-centered">
               <div class="info-main">
                 <div class="info-main-num">
-                  765
+                  {{ getPressureMm(weatherInfo?.main?.pressure) }}
                 </div>
                 <div class="info-main-text">
                   mm
@@ -85,7 +104,7 @@
           <div class="card-small-info">
             <div class="card-small-data">
               <div class="info-main-num">
-                21
+                {{ Math.round(weatherInfo?.main?.feels_like) }}
               </div>
               <div class="info-main-text">
                 °C
@@ -114,7 +133,7 @@
                   Sunrise
                 </div>
                 <div class="state-time">
-                  07:31:42
+                  {{ sunriseTime }}
                 </div>
               </div>
               <div class="state">
@@ -123,7 +142,7 @@
                   Sunset
                 </div>
                 <div class="state-time">
-                  18:34:19
+                  {{ sunsetTime }}
                 </div>
               </div>
             </div>
@@ -136,7 +155,7 @@
           <div class="card-small-info">
             <div class="card-small-data">
               <div class="info-main-num">
-                80
+                {{ weatherInfo?.clouds?.all }}
               </div>
               <div class="info-main-text">
                 %
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,15 @@
+const PRESSURE_UNITS = 0.750062
+
 export const capitalizeFirstLatter = (string) => {
   if (!string) return ''
 
   return string.charAt(0).toUpperCase() + string.slice(1, )
+}
+
+export const getPressureMm = (value) => {
+  return Math.round(value * PRESSURE_UNITS)
+}
+
+export const getTime = (seconds) => {
+  return new Date(seconds * 1000).toLocaleTimeString('ru-RU', { timeZone: 'Atlantic/Reykjavik' })
 }
